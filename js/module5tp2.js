a69fd6bb4c5e1a561af38538cbb89c906564a145
prnl <prnl.dev@gmail.com>
2019-07-01 14:46:09 +0200

    js repeter mot de passe

--- js/module5tp2.js
+++ js/module5tp2.js
@@ -1,6 +1,18 @@
 window.onload=function(){
     let firstname = document.getElementById("firstname");
+    let repeatPwd = document.getElementById("repeatpwd");
+    let pwd = document.getElementById("pwd");
+    let rigthColumn = document.getElementById("rightColumn");
+
     firstname.addEventListener("click",function(){
         alert("entrez votre prénom");
+    });
+    repeatPwd.addEventListener("blur", function(){
+        if (repeatPwd.value!=pwd.value){
+            let error = document.createElement("p");
+            error.textContent="le mot de passe n'est pas indentique";
+            error.style="border: 1px solid red";
+            rigthColumn.appendChild(error);
+        }
     });
 }
